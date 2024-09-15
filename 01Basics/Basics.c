dfdaa8d0c0537f809eef666a44f16a3e6b4ce006
rakhi prasad <142569721+Prasadrakhi2@users.noreply.github.com>
2024-09-15 17:42:22 +0000

    first c

---
+++ 01Basics/Basics.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main(){
+    printf("Hello"); //1
+
+    printf("\n");
+    printf("Sum= %d \n",7+8); //5
+
+    printf("Sub= %d\n",7-8); //6
+
+    printf("mul= %d\n",7*8); //7
+
+    printf("div= %d\n",16/8); //8
+
+    printf("remd= %d\n",7%8); //9
+
+    printf("sq= %d\n",7*7); //10
+
+    printf("cub= %d\n",7*7*7); //11
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+command to run 
+
+  gcc 01Basics/Basics.c -o Basics
+ ./Basics
+*/
